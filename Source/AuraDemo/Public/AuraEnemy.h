412017e9c5241582c35ae00dd0934aca434b236f
wangyiming <1091193455@qq.com>
2026-05-09 00:16:57 +0800

    create Characters!!

---
+++ Source/AuraDemo/Public/AuraEnemy.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacter.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURADEMO_API AAuraEnemy : public AAuraCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
