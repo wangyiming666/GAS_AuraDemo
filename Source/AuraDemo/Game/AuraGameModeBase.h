5612434574a9ff6a21f6797190a4278a03e31392
wangyiming <1091193455@qq.com>
2026-07-05 22:04:39 +0800

    2.10 GmeMode And Character Camera!!

--- Source/AuraDemo/Game/AuraGameModeBase.h
+++ Source/AuraDemo/Game/AuraGameModeBase.h
@@ -3,16 +3,18 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AuraCharacterBase.h"
-#include "AuraCharacter.generated.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURADEMO_API AAuraCharacter : public AAuraCharacterBase
+class AURADEMO_API AAuraGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-public:
-	AAuraCharacter();
+	
+	
+	
+	
 };
--- Source/AuraDemo/Private/AuraCharacter.cpp
+++ Source/AuraDemo/Private/AuraCharacter.cpp
@@ -3,6 +3,16 @@
 
 #include "AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
 
 
-
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0, 400, 0);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+}
