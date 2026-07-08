2262c77ecad918e62c4f4db3a2564f0b9b2c42e4
wangyiming <1091193455@qq.com>
2026-07-08 22:40:34 +0800

    3.2-3.4 playerState、 gas组件初始化

---
+++ Source/AuraDemo/Player/AuraPlayerState.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraPlayerState.h"
+
+
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
